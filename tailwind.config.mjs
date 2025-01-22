547513995410128068dde79f686aee66dec341db
Ghox19 <gonzalo.moncada19@gmail.com>
2025-01-22 16:03:25 -0300

    feat: movil fix with br in text

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			screens: {
+				'xs': '350px',
+				'c400': '400px',
+				'c500': '500px',
+				'c550': '550px',
+				'c1400': '1400px',
+			},
+		},
 	},
 	plugins: [],
 }
