19dd6552568cb8bcea3b1f8dc0a439fe50a2cb29
Francisco Sanchez <francisco.sanchez.g@usach.cl>
2025-02-09 16:14:22 -0300

    fix:fix the space between the buttons in nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,88 +37,79 @@ export function Navbar() {
 
             <div className="fixed bg-[#6610F2] text-white flex items-center z-40 w-full h-16 sm:h-24">
                 <nav className="grid grid-cols-12 gap-[15px] w-full c400:w-[85%] lg:w-[67%] mx-auto h-16 items-center text-center">
-                    <span className="col-start-2 c400:col-start-1 col-span-7 text-xl text-start lg:col-start-1 lg:col-span-4 lg:text-3xl text-extrabold my-auto">English Pronunciation</span>
-                    <a href="/nosotros" className="col-start-5 col-span-2 text-white text-bold text-lg c1800:text-xl hidden lg:block ">
-                        <span className="relative after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[2px] after:bg-white after:scale-x-0 after:origin-center after:transition-transform after:duration-300 hover:after:scale-x-100">
-                            Sobre nosotros
-                        </span>
+                    <a href="/" className="col-start-1 col-span-4 text-xl lg:text-3xl text-extrabold my-auto">
+                        <span>English Pronunciation</span>
                     </a>
-                    <a href="/objetivo" className="col-start-7 col-span-1 text-start  text-white text-bold text-lg c1800:text-xl hidden lg:block ">
-                        <span className="relative after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[2px] after:bg-white after:scale-x-0 after:origin-center after:transition-transform after:duration-300 hover:after:scale-x-100">
-                            Objetivo
-                        </span>
-                    </a>
-                    <a href="/ohno" className="col-start-8 col-span-2 text-white text-bold text-lg c1800:text-xl hidden lg:block">
-                        <span className="relative after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[2px] after:bg-white after:scale-x-0 after:origin-center after:transition-transform after:duration-300 hover:after:scale-x-100">
-                            Tu cerebro te engaña
-                        </span>
-                    </a>
-
-
-                    {/* Fonemas container */}
-                    <section className="col-start-10 col-span-1 mx-auto relative hidden lg:block">
-                        {/* NavBar fonemas button */}
-                        <button
-                            onClick={handleFonemasToggle}
-                            className="relative text-white text-bold text-lg c1800:text-xl hidden lg:block after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[2px] after:bg-white after:scale-x-0 after:origin-center after:transition-transform after:duration-300 hover:after:scale-x-100"
-                        >
-                            Letras
-                        </button>
-                        <div className={`absolute left-1/2 -translate-x-1/2 w-48 bg-[#6610F2]  rounded-md shadow-lg z-50 text-center 
+                    <div className="col-start-5 col-span-8 hidden lg:flex justify-evenly items-center">
+                        <a href="/nosotros" className="text-white text-bold text-lg c1800:text-xl">
+                            <span className="relative after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[2px] after:bg-white after:scale-x-0 after:origin-center after:transition-transform after:duration-300 hover:after:scale-x-100">
+                                Sobre nosotros
+                            </span>
+                        </a>
+                        <a href="/objetivo" className="text-white text-bold text-lg c1800:text-xl">
+                            <span className="relative after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[2px] after:bg-white after:scale-x-0 after:origin-center after:transition-transform after:duration-300 hover:after:scale-x-100">
+                                Objetivo
+                            </span>
+                        </a>
+                        <a href="/ohno" className="text-white text-bold text-lg c1800:text-xl">
+                            <span className="relative after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[2px] after:bg-white after:scale-x-0 after:origin-center after:transition-transform after:duration-300 hover:after:scale-x-100">
+                                Tu cerebro te engaña
+                            </span>
+                        </a>
+                        <section className="relative">
+                            <button onClick={handleFonemasToggle} className="text-white text-bold text-lg c1800:text-xl relative after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[2px] after:bg-white after:scale-x-0 after:origin-center after:transition-transform after:duration-300 hover:after:scale-x-100">
+                                Letras
+                            </button>
+                            <div className={`absolute left-1/2 -translate-x-1/2 w-48 bg-[#6610F2]  rounded-md shadow-lg z-50 text-center 
                             transition-all duration-200 ease-in-out origin-top
                             ${isFonemasOpen ? 'opacity-100 scale-y-100' : 'opacity-0 scale-y-0'}`}
-                        >
-                            {/* Dropdown options */}
-                            <div className="py-1 text-white">
-                                <a href="/fonema/z" className="block px-4 py-2 text-white hover:underline">
-                                    Letra Z
-                                </a>
-                                <a href="/fonema/t-d" className="block px-4 py-2 text-white hover:underline">
-                                    Letra T-D
-                                </a>
-                                <a href="/fonema/v-b" className="block px-4 py-2 text-white hover:underline">
-                                    Letra V-B
-                                </a>
-                                <a href="/fonema/h" className="block px-4 py-2 text-white hover:underline">
-                                    Letra H
-                                </a>
+                            >
+                                <div className="py-1 text-white">
+                                    <a href="/fonema/z" className="block px-4 py-2 text-white hover:underline">
+                                        Letra Z
+                                    </a>
+                                    <a href="/fonema/t-d" className="block px-4 py-2 text-white hover:underline">
+                                        Letra T-D
+                                    </a>
+                                    <a href="/fonema/v-b" className="block px-4 py-2 text-white hover:underline">
+                                        Letra V-B
+                                    </a>
+                                    <a href="/fonema/h" className="block px-4 py-2 text-white hover:underline">
+                                        Letra H
+                                    </a>
+                                </div>
                             </div>
-                        </div>
-                    </section>
-                    {/* Ejercicios container */}
-                    <section className="col-start-11 col-span-1 ml-auto relative hidden lg:block">
-                        {/* NavBar ejercicios button */}
-                        <button
-                            onClick={handleEjerciciosToggle}
-                            className="relative text-white text-bold text-lg c1800:text-xl hidden lg:block after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[2px] after:bg-white after:scale-x-0 after:origin-center after:transition-transform after:duration-300 hover:after:scale-x-100"
-                        >
-                            Ejercicios
-                        </button>
-                        {/* Dropdown options */}
-                        <div className={`absolute left-1/2 -translate-x-1/2 w-48 bg-[#6610F2] rounded-md shadow-lg z-50 text-center 
+                        </section>
+                        <section className="relative">
+                            <button onClick={handleEjerciciosToggle} className="text-white text-bold text-lg c1800:text-xl relative after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[2px] after:bg-white after:scale-x-0 after:origin-center after:transition-transform after:duration-300 hover:after:scale-x-100">
+                                Ejercicios
+                            </button>
+                            <div className={`absolute left-1/2 -translate-x-1/2 w-48 bg-[#6610F2] rounded-md shadow-lg z-50 text-center 
                             transition-all duration-200 ease-in-out origin-top
                             ${isEjerciciosOpen ? 'opacity-100 scale-y-100' : 'opacity-0 scale-y-0'}`}
-                        >
-                            <div className="py-1">
-                                <a href="/exercise/z" className="block px-4 py-2 text-white hover:underline">
-                                    Ejercicios Z
-                                </a>
-                                <a href="/exercise/t-d" className="block px-4 py-2 text-white hover:underline">
-                                    Ejercicios T-D
-                                </a>
-                                <a href="/exercise/v-b" className="block px-4 py-2 text-white hover:underline">
-                                    Ejercicios V-B
-                                </a>
-                                <a href="/exercise/h" className="block px-4 py-2 text-white hover:underline">
-                                    Ejercicios H
-                                </a>
+                            >
+                                <div className="py-1">
+                                    <a href="/exercise/z" className="block px-4 py-2 text-white hover:underline">
+                                        Ejercicios Z
+                                    </a>
+                                    <a href="/exercise/t-d" className="block px-4 py-2 text-white hover:underline">
+                                        Ejercicios T-D
+                                    </a>
+                                    <a href="/exercise/v-b" className="block px-4 py-2 text-white hover:underline">
+                                        Ejercicios V-B
+                                    </a>
+                                    <a href="/exercise/h" className="block px-4 py-2 text-white hover:underline">
+                                        Ejercicios H
+                                    </a>
+                                </div>
                             </div>
-                        </div>
-                    </section>
-
-                    <a href="/extras" className="relative text-white text-bold text-lg c1800:text-xl hidden lg:block after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[2px] after:bg-white after:scale-x-0 after:origin-center after:transition-transform after:duration-300 hover:after:scale-x-100">
-                        Extras
-                    </a>
+                        </section>
+                        <a href="/extras" className="text-white text-bold text-lg c1800:text-xl">
+                            <span className="relative after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[2px] after:bg-white after:scale-x-0 after:origin-center after:transition-transform after:duration-300 hover:after:scale-x-100">
+                                Extras
+                            </span>
+                        </a>
+                    </div>
                     <button onClick={handleMenuOpen} className="col-start-11 col-span-2 p-2 lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
