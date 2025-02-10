d02af261f6cc91efd067d06d5f9b864bf0db32b3
Francisco Sanchez <francisco.sanchez.g@usach.cl>
2025-02-10 01:02:39 -0300

    fix:fix ohno text in menu, exercise title and ohno final text

--- src/components/Exercise.jsx
+++ src/components/Exercise.jsx
@@ -1,13 +1,13 @@
 import { useState, useEffect } from "react";
 import AudioPlayer from "./AudioPlayer";
 
-export function Exercise({fonema, routeOfFonema, routeOfTitleSVG, text, answerA, textAnswerA, answerB, textAnswerB}) {
+export function Exercise({ fonema, routeOfFonema, routeOfTitleSVG, text, answerA, textAnswerA, answerB, textAnswerB }) {
     const [data, setData] = useState(null);
     const [currentAudio, setCurrentAudio] = useState(0);
     const [selectedButton, setSelectedButton] = useState(null);
     const [isCorrect, setIsCorrect] = useState(false);
 
-    const jsonFilePath = "/json/" + fonema + ".json";   
+    const jsonFilePath = "/json/" + fonema + ".json";
 
     const fetchData = async () => {
         const response = await fetch(jsonFilePath);
@@ -40,9 +40,9 @@ export function Exercise({fonema, routeOfFonema, routeOfTitleSVG, text, answerA,
     // Get button color function
     const getButtonColor = (buttonType) => {
         if (selectedButton === buttonType) {
-            return isCorrect ? 
-            "bg-green-500 animate-growShrink" 
-            : "bg-red-500 animate-shake";
+            return isCorrect ?
+                "bg-green-500 animate-growShrink"
+                : "bg-red-500 animate-shake";
         }
         return "bg-[#6610F2]";
     }
@@ -53,39 +53,39 @@ export function Exercise({fonema, routeOfFonema, routeOfTitleSVG, text, answerA,
                 <div className="grid grid-cols-12 gap-[15px] w-[85%] sm:w-[75%] mx-auto pt-2">
                     {/* Link to fonema /z/ */}
                     <a
-                    href={`${routeOfFonema}`}
-                    className="text-xs underline
+                        href={`${routeOfFonema}`}
+                        className="text-xs underline
                     col-span-12
                     sm:text-xl">
                         Letra {fonema}
                     </a>
                     {/* Title */}
                     <img
-                    src={`${routeOfTitleSVG}`}
-                    alt="Titulo de ejercicios" 
-                    className="w-[206px] h-[93px] object-contain
+                        src={`${routeOfTitleSVG}`}
+                        alt="Titulo de ejercicios"
+                        className="w-[206px] h-[93px] object-contain
                     col-span-6 col-start-4 place-self-center
-                    lg:w-[477px]]"
+                    lg:w-[477px] lg:h-40"
                     />
                 </div>
             </div>
             <div className="flex flex-col justify-around  h-[60vh]">
                 <section className="grid grid-cols-12 gap-[15px] w-[85%] sm:w-[75%] mx-auto">
                     {/* Left start */}
-                    <img src="/images/svg/global/Estrella.svg" 
-                    alt="Imagen de estrella"
-                    className="
+                    <img src="/images/svg/global/Estrella.svg"
+                        alt="Imagen de estrella"
+                        className="
                     col-span-2 row-span-2 col-start-1 row-start-1 place-self-end
                     lg:col-span-2 lg:row-span-1 lg:col-start-1 lg:row-start-1"
                     />
                     <div className="col-span-8 col-start-3 place-content-center">
                         {/* Text */}
-                        <div dangerouslySetInnerHTML={{__html: text}} />
+                        <div dangerouslySetInnerHTML={{ __html: text }} />
                     </div>
                     {/* Right start */}
-                    <img src="/images/svg/global/Estrella.svg" 
-                    alt="Imagen de estrella"
-                    className="scale-x-[-1]
+                    <img src="/images/svg/global/Estrella.svg"
+                        alt="Imagen de estrella"
+                        className="scale-x-[-1]
                     col-span-2 row-span-2 col-start-11 row-start-1 place-self-end
                     lg:col-span-2 lg:row-span-1 lg:col-start-11 lg:row-start-1"
                     />
@@ -93,7 +93,7 @@ export function Exercise({fonema, routeOfFonema, routeOfTitleSVG, text, answerA,
                 <section className="grid grid-cols-12 gap-[15px] w-[85%] sm:w-[75%] mx-auto">
                     {/* Number of word */}
                     <p
-                    className="text-[#6610F2] text-bold text-xl text-center mb-4
+                        className="text-[#6610F2] text-bold text-xl text-center mb-4
                     col-span-12
                     lg:text-3xl"
                     >
@@ -102,8 +102,8 @@ export function Exercise({fonema, routeOfFonema, routeOfTitleSVG, text, answerA,
                     {/* Left arrow button */}
                     {(currentAudio != 0) && (
                         <button
-                        onClick={discountAudio}
-                        className="
+                            onClick={discountAudio}
+                            className="
                         col-span-2 col-start-1"
                         >
                             <img src="/images/svg/global/Flecha_Izquierda.svg" alt="Imagen de flecha izquierda" className="w-[160px]" />
@@ -114,17 +114,17 @@ export function Exercise({fonema, routeOfFonema, routeOfTitleSVG, text, answerA,
                     w-[86px] lg:w-[164px]
                     col-span-4 col-start-5 place-self-center"
                     >
-                        {(!data)? (
-                            <AudioPlayer audio_path={``} className="lg:w-[164px]"/>
+                        {(!data) ? (
+                            <AudioPlayer audio_path={``} className="lg:w-[164px]" />
                         ) : (
-                            <AudioPlayer audio_path={`/audio/${fonema}/exercise/${data.audios[currentAudio].name}.mp3`} className="lg:w-[164px]"/>
+                            <AudioPlayer audio_path={`/audio/${fonema}/exercise/${data.audios[currentAudio].name}.mp3`} className="lg:w-[164px]" />
                         )}
                     </div>
                     {/* Right arrow button */}
                     {(data) && (currentAudio != data.audios.length - 1) && (
                         <button
-                        onClick={countAudio}
-                        className="
+                            onClick={countAudio}
+                            className="
                         col-span-2 col-start-11"
                         >
                             <img src="/images/svg/global/Flecha_Derecha.svg" alt="Imagen de flecha derecha" className="w-[160px]" />
@@ -134,8 +134,8 @@ export function Exercise({fonema, routeOfFonema, routeOfTitleSVG, text, answerA,
                 <section className="flex gap-[15px] w-[85%] sm:w-[35%] mx-auto justify-between">
                     {/* Option A button */}
                     <button
-                    onClick={() => validateAnswer(answerA)}
-                    className={`bg-[#6610F2] relative overflow-hidden rounded-full px-8 py-4 text-xl font-bold text-white transition-all duration-300 ease-in-out transform
+                        onClick={() => validateAnswer(answerA)}
+                        className={`bg-[#6610F2] relative overflow-hidden rounded-full px-8 py-4 text-xl font-bold text-white transition-all duration-300 ease-in-out transform
                     col-span-6 col-start-1
                     lg:col-span-4 lg:col-start-3
                     sm:w-[20vh]
@@ -144,9 +144,9 @@ export function Exercise({fonema, routeOfFonema, routeOfTitleSVG, text, answerA,
                         {textAnswerA}
                     </button>
                     {/* Option B button */}
-                    <button 
-                    onClick={() => validateAnswer(answerB)}
-                    className={`bg-[#6610F2] relative overflow-hidden rounded-full px-8 py-4 text-xl font-bold text-white transition-all duration-300 ease-in-out transform
+                    <button
+                        onClick={() => validateAnswer(answerB)}
+                        className={`bg-[#6610F2] relative overflow-hidden rounded-full px-8 py-4 text-xl font-bold text-white transition-all duration-300 ease-in-out transform
                     col-span-6 col-start-7
                     lg:col-span-4 lg:col-start-7
                     sm:w-[20vh]
@@ -155,7 +155,7 @@ export function Exercise({fonema, routeOfFonema, routeOfTitleSVG, text, answerA,
                         {textAnswerB}
                     </button>
                 </section>
-            </div>  
+            </div>
         </article>
     );
 }
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -10,26 +10,23 @@ export function Menu({ isMenuOpen, handleMenuOpen, isFonemasOpen, setIsFonemasOp
     return (
         <>
             <div
-            onClick={handleMenuOpen}
-            className={`fixed inset-0 bg-black bg-opacity-50 z-10 mt-16 transition-opacity duration-300 ${
-            isMenuOpen ? "opacity-100" : "opacity-0 pointer-events-none"
-            }`}
+                onClick={handleMenuOpen}
+                className={`fixed inset-0 bg-black bg-opacity-50 z-10 mt-16 transition-opacity duration-300 ${isMenuOpen ? "opacity-100" : "opacity-0 pointer-events-none"
+                    }`}
             ></div>
 
             <menu
-            className={`fixed top-0 left-0 w-full bg-[#6610f2] text-white z-20 transform transition-transform duration-300 ${
-            isMenuOpen ? "translate-y-0 animate-slideDown mt-16" : "-translate-y-full animate-slideUp"
-            }`}
+                className={`fixed top-0 left-0 w-full bg-[#6610f2] text-white z-20 transform transition-transform duration-300 ${isMenuOpen ? "translate-y-0 animate-slideDown mt-16" : "-translate-y-full animate-slideUp"
+                    }`}
             >
                 <nav className="flex flex-col justify-center text-right w-[85%] mx-auto p-4">
                     <a href="/nosotros" className="text-white text-bold text-xl">Quienes somos</a>
                     <a href="/" className="text-white text-bold text-xl mt-4">Objetivos</a>
-                    <a href="/ohno" className="text-white text-bold text-xl mt-4">Oh no, mi cerebro me engaña</a>
+                    <a href="/ohno" className="text-white text-bold text-xl mt-4">Tu cerebro te engaña</a>
                     <button onClick={handleFonemasToggle} className="text-white text-bold text-right text-xl mt-4">Fonemas</button>
                     <div
-                    className={`overflow-hidden transition-all duration-300 ease-in-out ${
-                    isFonemasOpen ? "max-h-screen opacity-100" : "max-h-0 opacity-0"
-                    }`}
+                        className={`overflow-hidden transition-all duration-300 ease-in-out ${isFonemasOpen ? "max-h-screen opacity-100" : "max-h-0 opacity-0"
+                            }`}
                     >
                         <div className="text-bold flex flex-col space-y-2 mt-2">
                             <a href="/fonema/z">Letra Z</a>
@@ -40,9 +37,8 @@ export function Menu({ isMenuOpen, handleMenuOpen, isFonemasOpen, setIsFonemasOp
                     </div>
                     <button onClick={handleEjerciciosToggle} className="text-white text-bold text-right text-xl mt-4">Ejercicios</button>
                     <div
-                    className={`overflow-hidden transition-all duration-300 ease-in-out ${
-                    isEjerciciosOpen ? "max-h-screen opacity-100" : "max-h-0 opacity-0"
-                    }`}
+                        className={`overflow-hidden transition-all duration-300 ease-in-out ${isEjerciciosOpen ? "max-h-screen opacity-100" : "max-h-0 opacity-0"
+                            }`}
                     >
                         <div className="text-bold flex flex-col space-y-2 mt-2">
                             <a href="/exercise/z">Ejercicios Z</a>
@@ -51,9 +47,9 @@ export function Menu({ isMenuOpen, handleMenuOpen, isFonemasOpen, setIsFonemasOp
                             <a href="/exercise/h">Ejercicios H</a>
                         </div>
                     </div>
-                    <a href="/" className="text-white text-bold text-xl my-4">Extras</a>
+                    <a href="/extras" className="text-white text-bold text-xl my-4">Extras</a>
                 </nav>
             </menu>
         </>
-      );
+    );
 }
